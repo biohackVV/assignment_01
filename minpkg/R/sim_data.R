685ebe45ee125d44473cb160b698654a4139c97b
victor <vvilla@berkeley.edu>
2018-02-16 12:40:20 -0800

    Update sim_data.R

--- minpkg/R/sim_data.R
+++ minpkg/R/sim_data.R
@@ -4,17 +4,19 @@
 #'
 #
 sim_data_scm <- function(n) {
-    # baseline covariate (already complete)
-    W <- runif(n, 0, 1)
-
-    # fill in the equation for A from question 1
-    #A <- rbinom(n, 1, p_A)
-
-    # fill in the equation for Y from question 1
-    #Y <- rbinom(n, 1, p_Y)
-
-    # assemble the generated data into a tibble
-    #data <- tibble::as_tibble()
-    #return(data)
+  # baseline covariate (already complete)
+  W <- runif(n, 0, 1)
+  
+  #!!! fill in the equation for A from question 1
+  A <- rbinom(n, 1, .10)  #p_A is undefined in question
+  
+  # fill in the equation for Y from question 1
+  ww <- (W - 1/2)^2  
+    
+  Y <- rbinom(n, 1, exp(ww)/(1+exp(ww)) )
+  
+  #!!! assemble the generated data into a tibble
+  data <- tibble::as_tibble(W,A,Y)
+  #return(data)
 }
 
